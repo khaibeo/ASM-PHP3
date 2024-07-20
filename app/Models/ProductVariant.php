0c3edff1a192a9336e2ede0a02a8bfa11f5c491a
khaibeo <khaimv13@gmail.com>
2024-07-20 13:31:36 +0700

    Sửa database phần biến thể + tạo model liên quan

---
+++ app/Models/ProductVariant.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductVariant extends Model
+{
+    use HasFactory;
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function attributeValues()
+    {
+        return $this->belongsToMany(AttributeValue::class, 'variant_attribute_values', 'variant_id', 'attribute_value_id');
+    }
+}
--- database/seeders/CartItemSeeder.php
+++ database/seeders/CartItemSeeder.php
@@ -14,23 +14,23 @@ class CartItemSeeder extends Seeder
      */
     public function run(): void
     {
-        $faker = Faker::create();
+        // $faker = Faker::create();
 
-        $cartIds = DB::table('carts')->pluck('id')->toArray();
-        $productVariantIds = DB::table('product_variants')->pluck('id')->toArray();
+        // $cartIds = DB::table('carts')->pluck('id')->toArray();
+        // $productVariantIds = DB::table('product_variants')->pluck('id')->toArray();
 
-        foreach ($cartIds as $cartId) {
-            $itemCount = $faker->numberBetween(1, 5);
+        // foreach ($cartIds as $cartId) {
+        //     $itemCount = $faker->numberBetween(1, 5);
 
-            for ($i = 0; $i < $itemCount; $i++) {
-                DB::table('cart_items')->insert([
-                    'cart_id' => $cartId,
-                    'product_variant_id' => $faker->randomElement($productVariantIds),
-                    'quantity' => $faker->numberBetween(1, 5),
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ]);
-            }
-        }
+        //     for ($i = 0; $i < $itemCount; $i++) {
+        //         DB::table('cart_items')->insert([
+        //             'cart_id' => $cartId,
+        //             'product_variant_id' => $faker->randomElement($productVariantIds),
+        //             'quantity' => $faker->numberBetween(1, 5),
+        //             'created_at' => now(),
+        //             'updated_at' => now(),
+        //         ]);
+        //     }
+        // }
     }
 }
