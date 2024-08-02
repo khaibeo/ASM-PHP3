052bce6aa2096779703e16bc76b91e024cf4371d
DucNM2004 <minhduc8883@gmail.com>
2024-08-02 10:41:59 +0700

    xóa cata bằng api

--- resources/views/admin/catalogue/catalogue_row.blade.php
+++ resources/views/admin/catalogue/catalogue_row.blade.php
@@ -1,9 +1,6 @@
+<!-- catalogue_row.blade.php -->
+
 <tr>
-    {{-- <th scope="row">
-            <div class="form-check">
-                <input class="form-check-input fs-15" type="checkbox" name="checkAll" value="option1">
-            </div>
-        </th> --}}
     <td>{{ $catalogue->id }}</td>
     <td>{{ $indent }}<a href="#!">{{ $catalogue->name }}</a></td>
     <td>{{ $catalogue->created_at }}</td>
@@ -11,37 +8,8 @@
     <td>
         <div class="d-flex gap-2">
             <a href="#" class="btn btn-warning">Sửa</a>
-            <form action="{{ route('admin.catalogues.destroy', ['id' => $catalogue->id]) }}" method="POST"
-                onsubmit="return confirm('Bạn có chắc chắn muốn xóa danh mục này?');">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger">
-                    Xóa
-                </button>
-            </form>
+            <button type="button" class="btn btn-danger btn-delete" data-id="{{ $catalogue->id }}">Xóa</button>
         </div>
-
-        {{-- <div class="dropdown d-inline-block">
-                <button class="btn btn-soft-secondary btn-sm dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="ri-more-fill align-middle"></i>
-                </button>
-                <ul class="dropdown-menu dropdown-menu-end">
-                    <li>
-                        <a href="{{ route('admin.catalogues.edit', ['id' => $catalogue->id]) }}" class="dropdown-item edit-item-btn">
-                            <i class="ri-pencil-fill align-bottom me-2 text-muted"></i> Edit
-                        </a>
-                    </li>
-                    <li>
-                        <form action="{{ route('admin.catalogues.destroy', ['id' => $catalogue->id]) }}" method="POST" onsubmit="return confirm('Bạn có chắc chắn muốn xóa danh mục này?');">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="dropdown-item remove-item-btn" style="background: none; border: none; padding: 0; color: inherit;">
-                                <i class="ri-delete-bin-fill align-bottom me-2 text-muted"></i> Delete
-                            </button>
-                        </form>
-                    </li>
-                </ul>
-            </div> --}}
     </td>
 </tr>
 @if ($catalogue->children)
